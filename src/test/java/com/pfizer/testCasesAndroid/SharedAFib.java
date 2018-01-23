1a6938e6ee15d84fcf268ab08cd8f48caf682a83
TestAccountGTHUB <34683998+TestAccountGTHUB@users.noreply.github.com>
2018-01-23 19:19:36 +0530

    Update SharedAFib.java

--- src/test/java/com/pfizer/testCasesAndroid/SharedAFib.java
+++ src/test/java/com/pfizer/testCasesAndroid/SharedAFib.java
@@ -275,10 +275,10 @@ public class SharedAFib extends BaseTest {
 			capabilities.setCapability("deviceName", deviceName);
 			capabilities.setCapability("platformName", "Android");
 			capabilities.setCapability("platformVersion", osVersion);
-			capabilities.setCapability("newCommandTimeout", "300000");
-			capabilities.setCapability("skipUnlock", "true");
-			capabilities.setCapability("unicodeKeyboard", "true");
-			capabilities.setCapability("resetKeyboard", "true");
+			capabilities.setCapability("newCommandTimeout", 300000);
+		        capabilities.setCapability("skipUnlock", true);
+		        capabilities.setCapability("unicodeKeyboard", true);
+		        capabilities.setCapability("resetKeyboard", true);
 			capabilities.setCapability("app",
 					System.getProperty("user.dir") + "/src/test/resources/APK/SharedAfib.apk");
 			capabilities.setCapability("appPackage", "com.pfizer.us.sharedafib");
